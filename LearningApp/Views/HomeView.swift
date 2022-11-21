cef9b98a7f321efeff865fb7af4fdd8ce68a0fa6
TeaTea <93224542+tranthanhhh@users.noreply.github.com>
2022-11-21 01:26:08 -0600

    - build the HomeView

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -12,18 +12,32 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack(alignment: .leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                ScrollView {
+                    LazyVStack {
+                        ForEach(model.modules) { module in
+                            VStack(spacing: 20) {
+                                // Learning Card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                // Test Card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Lessons", time: module.test.time)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
         }
-        .padding()
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
