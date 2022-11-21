c15aa29cee8111a2a4793326167d6e38bc69ae35
TeaTea <93224542+tranthanhhh@users.noreply.github.com>
2022-11-20 23:43:20 -0600

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Tran Thanh on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
